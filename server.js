5dc013784697d7e7ac10a731a5335705f0ffe012
Tatiane Sala <tatianedleal@gmail.com>
2024-02-04 17:59:19 -0500

    error handler

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3000;
 
 // Custom Middleware - Logger
 app.use(logger);
 
+
+// 3rd Party Middleware, eg: CORS (Cross-Origin Resource Sharing)
+// After development is done, remove || !origin and local domains from white list.
+const whiteOriginDomainList = ['http://localhost:3000', 'https://www.google.com'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        // If the origin domain is in the white list, then allow the request
+        if (whiteOriginDomainList.indexOf(origin) !== -1 || !origin) {
+            // null means no error and true means the origin is allowed
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 
 // 1 - Built-in Middleware to handle urlencoded data (form data) 'content-type: application/x-www-form-urlencoded'
 // so we can get this data here
@@ -65,11 +84,25 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-
 // A slash followed by anything will get to this endpoint that will serve the 404 page.
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// app.get('/*', (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// });
+
+// But for 404 (wildcard) we can actually use the following:
+// So as this is at the end of the page, after all possible routes, anything that made it here should get the 404 page.
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`, '- Click here: http://localhost:' + PORT);
