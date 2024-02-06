ed1ec8e69d7961ed7774c422705c5830bf90ac2a
Tatiane Sala <tatianedleal@gmail.com>
2024-02-06 14:56:29 -0500

    handling refresh tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,28 +4,25 @@ const usersDB = {
     this.users = data;
   },
 };
-
 const bcrypt = require("bcrypt");
+
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const fsPromises = require("fs").promises; // we are using fs for now as we are not using mongo or another real database
+const fsPromises = require("fs").promises;
 const path = require("path");
 
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
-  if (!user || !pwd) {
-    return res.status(400).json({ message: "User and password are required." });
-  }
-
-  const foundUser = usersDB.users.find((u) => u.username === user);
-
-  if (!foundUser) return res.sendStatus(401); // Unauthorized
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+  const foundUser = usersDB.users.find((person) => person.username === user);
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
   // evaluate password
-
   const match = await bcrypt.compare(pwd, foundUser.password);
-
   if (match) {
-    // Create JWTs
+    // create JWTs
     const accessToken = jwt.sign(
       { username: foundUser.username },
       process.env.ACCESS_TOKEN_SECRET,
@@ -36,10 +33,9 @@ const handleLogin = async (req, res) => {
       process.env.REFRESH_TOKEN_SECRET,
       { expiresIn: "1d" }
     );
-
-    // Saving refresh token with current user
+    // Saving refreshToken with current user
     const otherUsers = usersDB.users.filter(
-      (u) => u.username !== foundUser.username
+      (person) => person.username !== foundUser.username
     );
     const currentUser = { ...foundUser, refreshToken };
     usersDB.setUsers([...otherUsers, currentUser]);
@@ -49,14 +45,13 @@ const handleLogin = async (req, res) => {
     );
 
     res.cookie("jwt", refreshToken, {
-        httpOnly: true , maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
     });
+
     res.json({ accessToken });
   } else {
-    res.sendStatus(401); // Unauthorized
+    res.sendStatus(401);
   }
 };
 
-module.exports = {
-  handleLogin,
-};
+module.exports = { handleLogin };
--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,38 +1,32 @@
 const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const handleRefreshToken = (req, res) => {
-  const cookies = req.cookies;
-  if (!cookies?.jwt) return res.status(401);
+    const cookies = req.cookies;
 
-  console.log(cookies.jwt);
-  const refreshToken = cookies.jwt;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
 
-  const foundUser = usersDB.users.find((u) => u.refreshToken === refreshToken);
-
-  if (!foundUser) return res.sendStatus(403); // Forbidden
-
-  // evaluate jwt
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || foundUser.username !== decoded.username)
-      return res.sendStatus(403); // Forbidden
-    const accessToken = jwt.sign(
-      { username: decoded.username },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "30s" }
+    if (!foundUser) return res.sendStatus(403); //Forbidden 
+    // evaluate jwt 
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            const accessToken = jwt.sign(
+                { "username": decoded.username },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '30s' }
+            );
+            res.json({ accessToken })
+        }
     );
-    console.log(accessToken);
-    res.json({ accessToken });
-  });
-};
+}
 
-module.exports = {
-  handleRefreshToken,
-};
+module.exports = { handleRefreshToken }
