1f52926f3f61bd1425f4efd4e2c6f0419d7a548f
Tatiane Sala <tatianedleal@gmail.com>
2024-02-09 12:57:39 -0500

    createw new user and employee

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,45 +1,32 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-
-const fsPromises = require("fs").promises;
-const path = require("path");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
-  const { user, pwd } = req.body;
+  const { username, pwd } = req.body;
+  console.log("user, pwd? ", username, pwd);
 
-  if (!user || !pwd) {
+  if (!username || !pwd) {
     return res.status(400).json({ message: "User and password are required." });
   }
 
   // Check if user already exists
-  const existingUser = usersDB.users.find((u) => u.username === user);
-  if (existingUser) {
-    return res.status(409).json({ message: "User already exists." }); // Conflict
-  }
+  const duplicate = await User.findOne({ "username": username }).exec();
+
+  if (duplicate) return res.sendStatus(409); // Conflict
 
   try {
     // Encrypt password
     const hashedPwd = await bcrypt.hash(pwd, 10);
-    const newUser = { 
-      "username": user,
-      "roles": {"User": 2001},
+
+    // Cretae and Store the new user
+    const result = await User.create({ 
+      "username": username,
       "password": hashedPwd
-     };
-    usersDB.setUsers([...usersDB.users, newUser]);
+     });
 
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', "users.json"),
-        JSON.stringify(usersDB.users)
-    );
+     console.log(result);
 
-    console.log(usersDB.users);
-
-    res.status(201).json({"success": `New user ${user} created!`});
+    res.status(201).json({"success": `New user ${username} created!`});
 
   } catch (err) {
     res.status(500).json({ "message": err.message });
